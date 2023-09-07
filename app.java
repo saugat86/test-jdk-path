106f9b3677b7e381997cc6690de7bb76107f89a0
saugat86 <saugattripathi86@gmail.com>
2023-09-07 19:21:15 +0545

    test

---
+++ app.java
@@ -0,0 +1,9 @@
+ 
+public class app{      
+  public static void main(String[] argvs)   
+  {   
+    long millis = System.currentTimeMillis();  
+    java.util.Date date = new java.util.Date(millis);      
+    System.out.println(date);   
+  }      
+}
